d942b1f33b0dd785e30822340fc4ac62084f1dd2
Reshetniak, Valeriia <reshetnyak.valeriya@gmail.com>
2025-06-16 21:07:46 -0700

    still trying

--- contract-gen/vite.config.ts
+++ contract-gen/vite.config.ts
@@ -2,6 +2,5 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/YKPPEF0-ContractFormatter-557763939/",
   plugins: [react()],
 });
