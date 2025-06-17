cbd4cfef9ba29fdb63be6c7a587f09a6b7dadba7
Reshetniak, Valeriia <reshetnyak.valeriya@gmail.com>
2025-06-16 20:32:24 -0700

    Fix Vite base path

--- contract-gen/vite.config.ts
+++ contract-gen/vite.config.ts
@@ -2,5 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/YKPPEF0-ContractFormatter-557763939/",
   plugins: [react()],
 });
